963e2caf47f1c6a89b83a3128ba56c53e309746b
Racheal <orukele_racheal@yahoo.com>
2020-04-03 21:00:35 +0100

    improve styles

--- src/1-single-price-grid-component/Index.js
+++ src/1-single-price-grid-component/Index.js
@@ -3,34 +3,37 @@ import "./style.css";
 
 function Index() {
   return (
-    <div className="container">
-      <div className="header">
-        <h4>Join our community</h4>
-        <p className="first">30-day, hassle-free money back guarantee</p>
-        <p>
-          Gain access to our full library of tutorials along with expert code
-          reviews. <br />
-          Perfect for any developers who are serious about honing their skills.
-        </p>
-      </div>
-      <div className="both">
-        <div className="left">
-          <h5>Monthly Subscription</h5>
-          <span className="one">$29</span>
-          <span className="two">per month</span>
-          <p>Full access for less than $1 a day</p>
-          <button>Sign Up</button>
-        </div>
-        <div className="right">
-          <h5>Why us</h5>
+    <div className="wrapper">
+      <div className="container">
+        <div className="header">
+          <h4>Join our community</h4>
+          <p className="first">30-day, hassle-free money back guarantee</p>
           <p>
-            Tutorials by industry experts
-            <br /> Peer & expert code review
-            <br /> Coding exercises <br /> Access to our GitHub repos <br />{" "}
-            Community forum Flashcard decks
-            <br />
-            New videos every week
+            Gain access to our full library of tutorials along with expert code
+            reviews. <br />
+            Perfect for any developers who are serious about honing their
+            skills.
           </p>
+        </div>
+        <div className="both">
+          <div className="left">
+            <h5>Monthly Subscription</h5>
+            <span className="one">$29</span>
+            <span className="two">per month</span>
+            <p>Full access for less than $1 a day</p>
+            <button>Sign Up</button>
+          </div>
+          <div className="right">
+            <h5>Why us</h5>
+            <p>
+              Tutorials by industry experts
+              <br /> Peer & expert code review
+              <br /> Coding exercises <br /> Access to our GitHub repos <br />{" "}
+              Community forum Flashcard decks
+              <br />
+              New videos every week
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- src/1-single-price-grid-component/IndexMobile.js
+++ src/1-single-price-grid-component/IndexMobile.js
@@ -3,41 +3,42 @@ import "./styleMobile.css";
 
 function IndexMobile() {
   return (
-    // <div>
-    <div className="containerMobile">
-      <div className="headerMobile">
-        <h4>Join our community</h4>
-        <p className="first">30-day, hassle-free money back guarantee</p>
-        <p>
-          Gain access to our full library of tutorials along with expert code
-          reviews. <br />
-          Perfect for any developers who are serious about honing their skills.
-        </p>
-        {/* <p>
-        </p> */}
-      </div>
-      <div className="bothMobile">
-        <div className="leftMobile">
-          <h5>Monthly Subscription</h5>
-          <span className="one">$29</span>
-          <span className="two">per month</span>
-          <p>Full access for less than $1 a day</p>
-          <button>Sign Up</button>
-        </div>
-        <div className="rightMobile">
-          <h5>Why us</h5>
+    <div className="wrapperMobile">
+      <div className="containerMobile">
+        <div className="headerMobile">
+          <h4>Join our community</h4>
+          <p className="first">30-day, hassle-free money back guarantee</p>
           <p>
-            Tutorials by industry experts
-            <br /> Peer & expert code review
-            <br /> Coding exercises <br /> Access to our GitHub repos <br />{" "}
-            Community forum Flashcard decks
-            <br />
-            New videos every week
+            Gain access to our full library of tutorials along with expert code
+            reviews. <br />
+            Perfect for any developers who are serious about honing their
+            skills.
           </p>
+          {/* <p>
+        </p> */}
+        </div>
+        <div className="bothMobile">
+          <div className="leftMobile">
+            <h5>Monthly Subscription</h5>
+            <span className="one">$29</span>
+            <span className="two">per month</span>
+            <p>Full access for less than $1 a day</p>
+            <button>Sign Up</button>
+          </div>
+          <div className="rightMobile">
+            <h5>Why us</h5>
+            <p>
+              Tutorials by industry experts
+              <br /> Peer & expert code review
+              <br /> Coding exercises <br /> Access to our GitHub repos <br />{" "}
+              Community forum Flashcard decks
+              <br />
+              New videos every week
+            </p>
+          </div>
         </div>
       </div>
     </div>
-    // {/* </div> */}
   );
 }
 
